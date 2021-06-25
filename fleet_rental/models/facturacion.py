24b8ccab18c022996bf744758a192ddd2ed65156
serch1322 <sergio.rodriguez@itreingenierias.com>
2021-06-25 00:41:56 -0500

    Ajuste de Facturacion

--- fleet_rental/models/facturacion.py
+++ fleet_rental/models/facturacion.py
@@ -2,8 +2,13 @@
 
 from odoo import models, fields, api, _
 
-class FacturacionporVehiculo(models.Model):
+class FacturacionporRenta(models.Model):
     _inherit = ['account.move']
 
-    renta = fields.Many2one('car.rental.contract,',string="Renta de Vehiculo")
-    vehiculo = fields.Many2one('fleet.vehicle', string="Vehiculo")
+    renta = fields.Many2one('car.rental.contract,',string="Renta de Vehiculo", store=True, copy=False)
+
+class FacturacionporVehiculo(models.Model):
+    _inherit = ['account.move.line']
+
+    vehiculo = fields.Many2one('fleet.vehicle',string="Vehiculo", store=True, copy=False)
+    aditamento = fields.Many2one('car.tools',string="Aditamentos/Accesorios", store=True, copy=False)
