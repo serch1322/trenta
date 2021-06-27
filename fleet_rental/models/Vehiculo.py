79886ac31cc688d07c9a55dbfddbcfadcf3c5edd
serch1322 <sergio.rodriguez@itreingenierias.com>
2021-06-26 22:36:01 -0500

    Ajuste para Depreciación en Inventario

--- fleet_rental/models/Vehiculo.py
+++ fleet_rental/models/Vehiculo.py
@@ -17,6 +17,7 @@ class EntidadMatricula(models.Model):
     categoria = fields.Many2one('car.category',string="Categoria de Vehiculo")
     tipo = fields.Selection(related='categoria.tipo')
     depr = fields.Selection([('total', 'Depreciación Total (100%)'), ('parcial', 'Depreciación Parcial ($175,000)')],string="Tipo de Depreciación Fiscal",default=False)
+    inventario = fields.Many2one('account.account',string="Cuenta de Inventario")
     insurance_count = fields.Integer(compute="_compute_count_all", string="Seguro", store=True)
     tools_count = fields.Integer(compute="_compute_count_all", string="Accesorios/Aditamentos", store=True)
     facturas_count = fields.Integer(compute="_compute_count_all", string="Facturas", store=True)
