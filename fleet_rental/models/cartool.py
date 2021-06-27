892eb8d2e6442a9d2de4e1a14af863f856d746f9
serch1322 <sergio.rodriguez@itreingenierias.com>
2021-06-26 22:25:27 -0500

    Update cartool.py

--- fleet_rental/models/cartool.py
+++ fleet_rental/models/cartool.py
@@ -34,7 +34,7 @@ class CarTools(models.Model):
 
     def unlink(self):
         if self.id == True:
-            raise UserError('No se puede eliminar ningun Accesorio o Aditamento registrado!'
+            raise UserError('No se puede eliminar ningun Accesorio o Aditamento registrado!')
         return super(CarTools, self).unlink()
 
 
