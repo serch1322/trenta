3734bcf4b8adf33f1ef7dcae2a92519850a2d12d
serch1322 <sergio.rodriguez@itreingenierias.com>
2021-06-28 10:59:39 -0500

    Ajuste de Depreciacion de Aditamentos

--- fleet_rental/models/cartool.py
+++ fleet_rental/models/cartool.py
@@ -18,7 +18,7 @@ class CarTools(models.Model):
 
     marca = fields.Char(string="Marca")
     rent_price = fields.Float(string="Precio de Renta por Día")
-    modelo = fields.Many2one('tools.category',string="Modelo")
+    modelo = fields.Many2one('tools.modelo',string="Modelo")
     categoria = fields.Many2one('tools.categoria', string="Categoria")
     date_compra = fields.Date(string="Año de Compra")
     date_fabric = fields.Date(string="Año de Fabricacion")
@@ -26,6 +26,14 @@ class CarTools(models.Model):
     tipo = fields.Selection([('aditamento', 'Aditamento'), ('accesorio', 'Accesorio')],
                             string="Tipo", copy=False, required=True)
     car = fields.Many2one('fleet.vehicle', string="Vehículo Asociado")
+    tiempo_de_depreciacion = fields.Integer(string="Duración de Depreciación Contable", required=True)
+    periodo_de_depreciacion = fields.Selection([('1', 'Meses'), ('12', 'Años')], string='Periodo de Depreciación',
+                                               default='1')
+    depreciacion_contable = fields.Many2one('account.asset', string="Depreciación Contable",
+                                            context="{'form_view_ref':'account_asset.view_account_asset_form'}")
+    depreciacion_fiscal = fields.Many2one('account.asset', string="Depreciación Fiscal",
+                                          context="{'form_view_ref':'account_asset.view_account_asset_form'}")
+    residual_value = fields.Monetary(related='depreciacion_contable.book_value',string="Valor residual")
 
 
     def unlink(self):
@@ -37,10 +45,54 @@ class CarTools(models.Model):
     def vendido(self):
         self.state = 'vendido'
 
-    def cambiar_disponible(self):
+    def depreciacion(self):
+        vals = self.env['ir.sequence'].next_by_code('secuencia.aditamentos')
         self.state = 'disponible'
-        vals['num_eco'] = self.env['ir.sequence'].next_by_code('secuencia.aditamentos')
-        return super(CarTools, self).create(vals)
+        self.ensure_one()
+        if self.tipo == 'aditamento':
+            activo = self.env['account.asset']
+            valores_activo = {}
+            valores_activo.update({
+                'name': '%s %s Fiscal' % (self.name,self.categoria.name),
+                'original_value': self.costo,
+                'acquisition_date': date.today(),
+                'method': 'linear',
+                'method_period': '1',
+                'first_depreciation_date': date.today(),
+                'account_asset_id': self.categoria.activo.id,
+                'account_depreciation_id': self.categoria.amortizacion.id,
+                'account_depreciation_expense_id': self.categoria.gasto.id,
+                'journal_id': self.categoria.diario.id,
+                'state': 'draft',
+                'asset_type': 'purchase',
+                'method_number': 48,
+                'aditamento': self.id,
+            })
+            activo_creado = activo.create(valores_activo)
+            self.depreciacion_fiscal = activo_creado.id
+            valores_contable={}
+            valores_contable.update({
+                'name': '%s %s Contable' %(self.name,self.modelo.name),
+                'original_value': self.costo,
+                'acquisition_date': date.today(),
+                'method': 'linear',
+                'method_period': self.periodo_de_depreciacion,
+                'first_depreciation_date': date.today(),
+                'account_asset_id': self.modelo.activo.id,
+                'account_depreciation_id': self.modelo.amortizacion.id,
+                'account_depreciation_expense_id': self.modelo.gasto.id,
+                'journal_id': self.modelo.diario.id,
+                'state': 'draft',
+                'salvage_value': self.costo * .15,
+                'asset_type': 'purchase',
+                'method_number': self.tiempo_de_depreciacion,
+                'aditamento': self.id,
+            })
+            contable_creado = activo.create(valores_contable)
+            self.depreciacion_contable = contable_creado.id
+            self.num_eco = vals
+        else:
+            self.num_eco = vals
 
 
     def _get_responsible_for_approval(self):
--- fleet_rental/models/categoria_tools.py
+++ fleet_rental/models/categoria_tools.py
@@ -3,16 +3,34 @@
 from datetime import date
 from odoo import models, fields, api, _
 
-class toolscategory(models.Model):
-    _name = 'tools.category'
+class ToolsModelo(models.Model):
+    _name = 'tools.modelo'
     _inherit = ['mail.thread', 'mail.activity.mixin']
 
     name = fields.Char(string="Modelo", required=True)
     activo = fields.Many2one('account.account', string="Cuenta de Activo", required=True)
     amortizacion = fields.Many2one('account.account', string="Cuenta de Amortizacion", required=True)
     gasto = fields.Many2one('account.account', string="Cuenta de Gasto", required=True)
+    diario = fields.Many2one('account.journal', string="Diario", required=True, domain="[('type', '=', 'general')]")
+    conteo_aditamento = fields.Integer(string="# Vehiculos", compute='conteo_de_aditamento')
 
-class toolscategory(models.Model):
+    def conteo_de_aditamento(self):
+        conteo = self.env['car.tools']
+        for record in self:
+            record.conteo_aditamento = conteo.search_count([('modelo','=',record.id)])
+
+
+    def abrir_aditamentos(self):
+        self.ensure_one()
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Aditamentos/Accesorios',
+            'view_mode': 'tree',
+            'res_model': 'car.tools',
+            'domain': [('modelo', '=', self.id)],
+        }
+
+class ToolsCategoria(models.Model):
     _name = 'tools.categoria'
     _inherit = ['mail.thread', 'mail.activity.mixin']
 
@@ -20,3 +38,20 @@ class toolscategory(models.Model):
     activo = fields.Many2one('account.account', string="Cuenta de Activo", required=True)
     amortizacion = fields.Many2one('account.account', string="Cuenta de Amortizacion", required=True)
     gasto = fields.Many2one('account.account', string="Cuenta de Gasto", required=True)
+    diario = fields.Many2one('account.journal', string="Diario", required=True, domain="[('type', '=', 'general')]")
+    conteo_aditamento = fields.Integer(string="# Vehiculos", compute='conteo_de_aditamento')
+
+    def conteo_de_aditamento(self):
+        conteo = self.env['car.tools']
+        for record in self:
+            record.conteo_aditamento = conteo.search_count([('categoria', '=', record.id)])
+
+    def abrir_aditamentos(self):
+        self.ensure_one()
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Aditamentos/Accesorios',
+            'view_mode': 'tree',
+            'res_model': 'car.tools',
+            'domain': [('categoria', '=', self.id)],
+        }
