5e65a93abd8247713034d635bf84b0e2ec1acecf
serch1322 <sergio.rodriguez@itreingenierias.com>
2021-07-12 16:27:52 -0500

    Update car_rental.py

--- fleet_rental/models/car_rental.py
+++ fleet_rental/models/car_rental.py
@@ -197,10 +197,10 @@ class CarRentalContract(models.Model):
             start_date = self.rent_start_date
         else:
             start_date = self.siguiente_fecha_de_factura
-        start_date_day = int(start_date.day)
+        start_date_day = datetime(start_date.day)
         next_month = datetime(start_date.year, start_date.month + 1, 1)
         end_date_month = datetime(start_date.year, start_date.month, calendar.mdays[start_date.month])
-        end_date_day = int(end_date_month.day)
+        end_date_day = datetime(end_date_month.day)
         if self.state == 'running':
             self.siguiente_fecha_de_factura = next_month
             if self.cost_frequency == 'monthly':
