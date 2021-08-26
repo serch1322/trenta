b67ce94c5e75ba3cbcd69f76c4f02176f37e35b1
joseluismc96 <72410117+joseluismc96@users.noreply.github.com>
2021-08-26 17:43:57 -0500

    Update licencia_contacto.py

--- fleet_rental/models/licencia_contacto.py
+++ fleet_rental/models/licencia_contacto.py
@@ -8,7 +8,7 @@ class HeredadContacto(models.Model):
     tipo_de_licencia = fields.Selection([('motociclista','Motociclista'),('automovilista','Automovilista'),
                                          ('chofer','Chofer'),('federal','Transporte Federal')],
                                         string="Tipo de Licencia")
-    conductor = fields.Boolean(string="¿Es Conductor?", default=False)
+    conductor = fields.Boolean(string="¿Es Conductor?", default=False, store=True)
     vigencia = fields.Date(string="Vigencia de Licencia")
     sexo = fields.Selection([('masculino', 'Masculino'), ('femenino', 'Femenino')], string="Sexo", copy=False)
     edad = fields.Integer(string="Edad de Conductor")
