3a5cc1f940e96c05b95202c2fdf60a63ef337811
joseluismc96 <72410117+joseluismc96@users.noreply.github.com>
2021-08-26 17:41:08 -0500

    ajuste

--- fleet_rental/models/licencia_contacto.py
+++ fleet_rental/models/licencia_contacto.py
@@ -8,13 +8,13 @@ class HeredadContacto(models.Model):
     tipo_de_licencia = fields.Selection([('motociclista','Motociclista'),('automovilista','Automovilista'),
                                          ('chofer','Chofer'),('federal','Transporte Federal')],
                                         string="Tipo de Licencia")
-    conductor = fields.Boolean(string="¿Es Conductor?", default=False, required=True)
+    conductor = fields.Boolean(string="¿Es Conductor?", default=False)
     vigencia = fields.Date(string="Vigencia de Licencia")
     sexo = fields.Selection([('masculino', 'Masculino'), ('femenino', 'Femenino')], string="Sexo", copy=False)
-    edad = fields.Integer(string="Edad de Conductor", required= True)
+    edad = fields.Integer(string="Edad de Conductor")
     tipo_sangre = fields.Selection([('o+', 'O+'), ('o-', 'O-'), ('a+', 'A+'), ('a-', 'A-'), ('b+', 'B+'),
                                     ('b-', 'B-'), ('ab+', 'AB+'), ('ab-', 'AB-')], string="Tipo de Sangre", copy=False)
-    numero_emergencia = fields.Char(string="Numero de Emergencia", required=True)
+    numero_emergencia = fields.Char(string="Numero de Emergencia")
 
     curp = fields.Char(string="CURP de Chofer")
 
