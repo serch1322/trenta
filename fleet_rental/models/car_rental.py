4b92ba650fdee79778583d7105a40ac174ed746e
joseluismc96 <72410117+joseluismc96@users.noreply.github.com>
2021-08-30 17:38:17 -0500

    Update car_rental.py

--- fleet_rental/models/car_rental.py
+++ fleet_rental/models/car_rental.py
@@ -52,7 +52,8 @@ class CarRentalContract(models.Model):
     customer_id = fields.Many2one('res.partner', required=True, string='Cliente', help="Customer")
     vehicle_id = fields.Many2one('fleet.vehicle', string="Vehiculo", required=True, help="Vehicle", copy=False,
                                  readonly=True,
-                                 states={'draft': [('readonly', False)]}
+                                 states={'draft': [('readonly', False)]},
+                                 domain=[('state_id','=','fleet_rental.vehicle_state_active')]
                                  )
     car_brand = fields.Many2one('fleet.vehicle.model.brand', string="Marca Vehiculo", size=50,
                                 related='vehicle_id.model_id.brand_id', store=True, readonly=True)
