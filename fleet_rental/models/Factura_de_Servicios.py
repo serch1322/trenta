73abe3aa35c7036c52d459404382ead453fd1212
serch1322 <sergio.rodriguez@itreingenierias.com>
2021-09-02 17:11:45 -0500

    Ajuste de Servicios

--- fleet_rental/models/Factura_de_Servicios.py
+++ fleet_rental/models/Factura_de_Servicios.py
@@ -12,7 +12,10 @@ class ServicioaFactura(models.Model):
     paga_cliente = fields.Boolean(string="Servicio pagado por Cliente",default=False, copy=False)
 
     def validar(self):
-        self.state = 'proceso'
+        if self.paga_cliente == True:
+            self.state = 'facturado'
+        else:
+            self.state = 'proceso'
 
     def unlink(self):
         if self.state == 'proceso' or self.state == 'facturado':
@@ -21,29 +24,30 @@ class ServicioaFactura(models.Model):
 
 
     def crear_factura_servicio(self):
-        if self.paga_cliente == True:
-            self.state = 'facturado'
-        else:
-            self.state = 'facturado'
-            self.ensure_one()
-            factu_prov = self.env['account.move']
-            valores_factu_prov = {}
+        mantenimiento = self.env['product.product'].search([("id", "=", "mantenimiento")])
+        self.state = 'facturado'
+        self.ensure_one()
+        factu_prov = self.env['account.move']
+        valores_factu_prov = {}
+        valores_factu_prov.update({
+            'partner_id': self.vendor_id.id,
+            'invoice_date': self.date,
+            'move_type': 'in_invoice',
+        })
+        lista_factu = []
+        lineas_factu = {
+            'product_id':mantenimiento,
+            'name': self.description,
+            'quantity': 1,
+            'price_unit': self.amount,
+            'tax_ids': mantenimiento.taxes_id,
+            'product_uom_id': mantenimiento.uom_id.id,
+            'account_id':mantenimiento.property_account_expense_id.id,
+        }
+        lista_factu.append((0, 0, lineas_factu))
+        if lista_factu:
             valores_factu_prov.update({
-                'partner_id': self.vendor_id.id,
-                'invoice_date': self.date,
-                'ref': self.description,
-                'move_type': 'in_invoice',
+                'invoice_line_ids': lista_factu,
             })
-            lista_factu = []
-            lineas_factu = {
-                'name': self.service_type_id.name,
-                'quantity': 1,
-                'price_unit': self.amount,
-            }
-            lista_factu.append((0, 0, lineas_factu))
-            if lista_factu:
-                valores_factu_prov.update({
-                    'invoice_line_ids': lista_factu,
-                })
-            factura_creada = factu_prov.create(valores_factu_prov)
+        factura_creada = factu_prov.create(valores_factu_prov)
 
