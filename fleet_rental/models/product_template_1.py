ef76ae22df5a685f4c4b307f6613f501ead77d6f
joseluismc96 <72410117+joseluismc96@users.noreply.github.com>
2021-09-09 12:24:22 -0500

    update

--- fleet_rental/models/product_template_1.py
+++ fleet_rental/models/product_template_1.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api, _
+
+from odoo import SUPERUSER_ID, _, api, fields, models
+from odoo.exceptions import UserError
 from odoo.tools.float_utils import float_compare, float_is_zero, float_round
-import logging
+
 
 class Producto(models.Model):
     _inherit = ['product.template']
