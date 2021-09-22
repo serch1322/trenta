94600b7023e095b38c10a228c17c68513d42afe9
joseluismc96 <72410117+joseluismc96@users.noreply.github.com>
2021-09-21 19:47:28 -0500

    update

--- fleet_rental/models/product_template_1.py
+++ fleet_rental/models/product_template_1.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api, _
-import logging
+from odoo import SUPERUSER_ID, _, api, fields, models
+from odoo.exceptions import UserError
+from odoo.tools.float_utils import float_compare, float_is_zero, float_round
 
 class Producto(models.Model):
     _inherit = ['product.template']
@@ -22,6 +23,7 @@ class ProductoProducto(models.Model):
 class registrarRecepcion(models.Model):
     _inherit = ['stock.picking']
 
+    flota = fields.Boolean(string="Es Flota", default=False, store=True)
     state = fields.Selection([
         ('draft', 'Draft'),
         ('waiting', 'Waiting Another Operation'),
@@ -108,6 +110,100 @@ class registrarRecepcion(models.Model):
                             'registrado': True,
                         }
                         locomprado_creado = orden.write(lineas_compradas)
+
+    def button_validate(self):
+        orden = self.env['purchase.order'].search([("name", "=", self.origin)])
+        for order in orden.order_line:
+            if order.product_id.tipo_products == 'aditamento' or order.product_id.tipo_products == 'accesorio' or order.product_id.tipo_products == 'vehiculo':
+                nombre_orden = {
+                    'flota': True,
+                }
+            else:
+                nombre_orden = {
+                    'flota': False,
+                }
+            locomprado_creado = self.write(nombre_orden)
+        # Clean-up the context key at validation to avoid forcing the creation of immediate
+        # transfers.
+        ctx = dict(self.env.context)
+        ctx.pop('default_immediate_transfer', None)
+        self = self.with_context(ctx)
+
+        # Sanity checks.
+        pickings_without_moves = self.browse()
+        pickings_without_quantities = self.browse()
+        pickings_without_lots = self.browse()
+        products_without_lots = self.env['product.product']
+        for picking in self:
+            if not picking.move_lines and not picking.move_line_ids:
+                pickings_without_moves |= picking
+
+            picking.message_subscribe([self.env.user.partner_id.id])
+            picking_type = picking.picking_type_id
+            precision_digits = self.env['decimal.precision'].precision_get('Product Unit of Measure')
+            no_quantities_done = all(
+                float_is_zero(move_line.qty_done, precision_digits=precision_digits) for move_line in
+                picking.move_line_ids.filtered(lambda m: m.state not in ('done', 'cancel')))
+            no_reserved_quantities = all(
+                float_is_zero(move_line.product_qty, precision_rounding=move_line.product_uom_id.rounding) for move_line
+                in picking.move_line_ids)
+            if no_reserved_quantities and no_quantities_done:
+                pickings_without_quantities |= picking
+
+            if picking_type.use_create_lots or picking_type.use_existing_lots:
+                lines_to_check = picking.move_line_ids
+                if not no_quantities_done:
+                    lines_to_check = lines_to_check.filtered(
+                        lambda line: float_compare(line.qty_done, 0, precision_rounding=line.product_uom_id.rounding))
+                for line in lines_to_check:
+                    product = line.product_id
+                    if product and product.tracking != 'none':
+                        if not line.lot_name and not line.lot_id:
+                            pickings_without_lots |= picking
+                            products_without_lots |= product
+
+        if not self._should_show_transfers():
+            if pickings_without_moves:
+                raise UserError(_('Please add some items to move.'))
+            if pickings_without_quantities:
+                raise UserError(self._get_without_quantities_error_message())
+            if pickings_without_lots:
+                raise UserError(_('You need to supply a Lot/Serial number for products %s.') % ', '.join(
+                    products_without_lots.mapped('display_name')))
+        else:
+            message = ""
+            if pickings_without_moves:
+                message += _('Transfers %s: Please add some items to move.') % ', '.join(
+                    pickings_without_moves.mapped('name'))
+            if pickings_without_quantities:
+                message += _(
+                    '\n\nTransfers %s: You cannot validate these transfers if no quantities are reserved nor done. To force these transfers, switch in edit more and encode the done quantities.') % ', '.join(
+                    pickings_without_quantities.mapped('name'))
+            if pickings_without_lots:
+                message += _('\n\nTransfers %s: You need to supply a Lot/Serial number for products %s.') % (
+                ', '.join(pickings_without_lots.mapped('name')),
+                ', '.join(products_without_lots.mapped('display_name')))
+            if message:
+                raise UserError(message.lstrip())
+
+        # Run the pre-validation wizards. Processing a pre-validation wizard should work on the
+        # moves and/or the context and never call `_action_done`.
+        if not self.env.context.get('button_validate_picking_ids'):
+            self = self.with_context(button_validate_picking_ids=self.ids)
+        res = self._pre_action_done_hook()
+        if res is not True:
+            return res
+
+        # Call `_action_done`.
+        if self.env.context.get('picking_ids_not_to_backorder'):
+            pickings_not_to_backorder = self.browse(self.env.context['picking_ids_not_to_backorder'])
+            pickings_to_backorder = self - pickings_not_to_backorder
+        else:
+            pickings_not_to_backorder = self.env['stock.picking']
+            pickings_to_backorder = self
+        pickings_not_to_backorder.with_context(cancel_backorder=True)._action_done()
+        pickings_to_backorder.with_context(cancel_backorder=False)._action_done()
+        return True
 
 class QuitarrecibirProductos(models.Model):
     _inherit = ['purchase.order']
