4796283c61f3a3d5a4373e147da08d1930aadf7e
serch1322 <sergio.rodriguez@itreingenierias.com>
2021-12-09 16:51:38 -0600

    Update car_rental.py

--- fleet_rental/models/car_rental.py
+++ fleet_rental/models/car_rental.py
@@ -491,9 +491,7 @@ class CarRentalContract(models.Model):
                                                                        'date_to': self.rent_end_date,
                                                                        'reserved_obj': self.vehicle_id.id
                                                                        })
-            self.write({'reserved_fleet_id': reserved_id.id})
-        else:
-            raise Warning('Sorry This vehicle is already booked by another customer')
+        self.write({'reserved_fleet_id': reserved_id.id})
         self.state = "reserved"
         sequence_code = 'secuencia.renta.de.carro'
         order_date = self.create_date
