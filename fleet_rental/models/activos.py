dbc6ac2f95ec8574b50192bd127008a1029f6514
serch1322 <51964536+serch1322@users.noreply.github.com>
2025-07-10 12:19:32 -0600

    Delete fleet_rental directory

--- fleet_rental/models/activos.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api, _
-
-class ActivosVehiculos(models.Model):
-    _inherit = ['account.asset']
-
-    vehiculo = fields.Many2one('fleet.vehicle',string="Vehiculo")
-    aditamento = fields.Many2one('car.tools', string="Aditamento")
